4b2be081bc3bd2057b8789ad31edd8de5ad81e97
Abhilash04 <abhilash04sharma@live.com>
2016-04-24 10:52:58 +0530

    Added New Apps File

---
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.practiceset2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7*8;
+        int actualHours = (weekday*5) + (weekend*2);
+        int solution = optimalHours - actualHours ;
+        display(solution);
+
+
+    }
+
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
+    }
+}
